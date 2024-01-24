deed20c369b2ca7e3f5843f03b344db6e6fbeac0
SarimNaeem009 <155876372+SarimNaeem009@users.noreply.github.com>
2024-01-24 22:13:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var length = 30;
+var width = 90;
+
+var area = (length*width);
+console.log(area)
+
+
+let userNamed = prompt("Your Name");
+alert("Welcome to my page.")
+
+console.log(userNamed);
